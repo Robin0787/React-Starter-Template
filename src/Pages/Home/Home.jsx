25e8b4845f02b8046b67631b07a331388ca4c436
Robin <mohammadrobin636@gmail.com>
2023-08-15 21:22:11 +0600

    First commit

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+    return (
+        <div className='text-center h-[calc(100vh-60px)] flex justify-center items-center text-5xl font-thin'>
+            <h1>Home</h1>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+
+const Navbar = () => {
+    return (
+        <div className='text-center bg-blue-500 h-[60px] flex justify-center items-center  text-white'>
+            <h1 className="text-2xl">Navbar</h1>
+        </div>
+    );
+};
+
+export default Navbar;
